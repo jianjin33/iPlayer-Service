9ff1f493c5504cae0d5b5e5a7ad887a3d17f44a3
tangjuanjuan <13718858630@163.com>
2017-09-25 01:37:56 +0800

    用户登陆模拟编码

---
+++ src/main/java/com/iplayer/media/api/dao/LoginDao.java
@@ -0,0 +1,12 @@
+package com.iplayer.media.api.dao;
+
+import com.iplayer.media.api.entity.UserBean;
+import com.iplayer.media.common.AbstractDao;
+
+/**
+ * Created by jianzuming on 17/9/24.
+ */
+
+public interface LoginDao extends AbstractDao<UserBean> {
+
+}
